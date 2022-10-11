6f316db8fdd69ef0ff1fd75a7b097ea777a851e9
Wilhelm Oks <>
2022-10-11 13:23:00 +0200

    TabView code improvements

--- SwiftUIRant/UI/Screens/InternalView.swift
+++ SwiftUIRant/UI/Screens/InternalView.swift
@@ -11,10 +11,20 @@ struct InternalView: View {
     @ObservedObject var appState = AppState.shared
     @ObservedObject var dataStore = DataStore.shared
     
-    enum Tab: Int, Hashable {
+    enum Tab: Int, CaseIterable, Hashable, Identifiable {
         case feed
         case notifications
         case settings
+        
+        var id: Int { rawValue }
+        
+        var displayName: String {
+            switch self {
+            case .feed:             return "Feed"
+            case .notifications:    return "Notifications"
+            case .settings:         return "Settings"
+            }
+        }
     }
     
     @State private var tab: Tab = .feed
@@ -35,45 +45,58 @@ struct InternalView: View {
     
     @ViewBuilder private func content() -> some View {
         TabView(selection: $tab) {
-            NavigationStack(path: $appState.navigationPath) {
-                FeedView()
+            ForEach(Tab.allCases) { tab in
+                tabView(tab)
             }
-            .tabItem {
-                Label {
-                    Text("Feed")
-                } icon: {
-                    Image(systemName: "list.bullet.rectangle")
-                }
-            }
-            .tag(Tab.feed)
-            .toolbarsVisible()
-            
-            NavigationStack() {
-                NotificationsView()
-            }
-            .tabItem {
-                Label {
-                    Text("Notifications (\(dataStore.numberOfUnreadNotifications))") //TODO: make a proper number badge
-                } icon: {
-                    Image(systemName: "bell")
-                }
-            }
-            .tag(Tab.notifications)
-            .toolbarsVisible()
-            
-            NavigationStack() {
-                SettingsView()
-            }
-            .tabItem {
-                Label {
-                    Text("Settings")
-                } icon: {
-                    Image(systemName: "gear")
-                }
-            }
-            .tag(Tab.settings)
-            .toolbarsVisible()
         }
+    }
+    
+    @ViewBuilder private func contentForTab(_ tab: Tab) -> some View {
+        switch tab {
+        case .feed:             FeedView()
+        case .notifications:    NotificationsView()
+        case .settings:         SettingsView()
+        }
+    }
+    
+    @ViewBuilder private func tabContent(_ tab: Tab) -> some View {
+        switch tab {
+        case .feed:
+            NavigationStack(path: $appState.navigationPath) {
+                contentForTab(tab)
+            }
+        default:
+            NavigationStack() {
+                contentForTab(tab)
+            }
+        }
+    }
+    
+    @ViewBuilder private func tabIcon(_ tab: Tab) -> some View {
+        switch tab {
+        case .feed:
+            Image(systemName: "list.bullet.rectangle")
+        case .notifications:
+            Image(systemName: "bell")
+        case .settings:
+            Image(systemName: "gear")
+        }
+    }
+    
+    @ViewBuilder private func tabView(_ tab: Tab) -> some View {
+        //TODO: make a proper number badge
+        let title = tab == .notifications ? "\(tab.displayName) (\(dataStore.numberOfUnreadNotifications))" : tab.displayName
+        
+        tabContent(tab)
+        .tabItem {
+            Label {
+                Text(title)
+            } icon: {
+                tabIcon(tab)
+            }
+        }
+        .tag(tab)
+        .toolbarsVisible()
     }
 }
 

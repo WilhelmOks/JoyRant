c0b16028c21fe4fa09616d1e9f0abd974f8b2902
Wilhelm Oks <>
2022-10-21 20:24:12 +0200

    cancel button for comment sheet

--- SwiftUIRant/UI/Screens/WriteComment/WriteCommentView.swift
+++ SwiftUIRant/UI/Screens/WriteComment/WriteCommentView.swift
@@ -8,27 +8,59 @@
 import SwiftUI
 
 struct WriteCommentView: View {
+    @Environment(\.presentationMode) private var presentationMode
+    
     @StateObject private var viewModel: WriteCommentViewModel = .init()
     
-    //TODO: add a close button
     //TODO: add image preview
     
     var body: some View {
         NavigationStack {
-            VStack(spacing: 10) {
-                TextEditor(text: .constant("Placeholder"))
-                    .foregroundColor(.primaryForeground)
-                    .overlay {
-                        RoundedRectangle(cornerRadius: 5, style: .continuous)
-                            .stroke()
-                            .foregroundColor(.secondaryForeground)
-                    }
-            }
+            content()
             .padding()
             .navigationTitle("Comment")
             .navigationBarTitleDisplayModeInline()
             .frame(minWidth: 320, minHeight: 300)
+            .toolbar {
+                cancelToolbarItem()
+            }
         }
+    }
+    
+    @ViewBuilder private func content() -> some View {
+        VStack(alignment: .leading, spacing: 10) {
+            TextEditor(text: .constant("Placeholder"))
+                .foregroundColor(.primaryForeground)
+                .overlay {
+                    RoundedRectangle(cornerRadius: 5, style: .continuous)
+                        .stroke()
+                        .foregroundColor(.secondaryForeground)
+                }
+        }
+    }
+    
+    @ViewBuilder private func cancelButton() -> some View {
+        Button {
+            presentationMode.wrappedValue.dismiss()
+        } label: {
+            Label {
+                Text("Cancel")
+            } icon: {
+                Image(systemName: "xmark")
+            }
+        }
+    }
+    
+    private func cancelToolbarItem() -> some ToolbarContent {
+        #if os(iOS)
+        ToolbarItem(placement: .navigationBarLeading) {
+            cancelButton()
+        }
+        #else
+        ToolbarItem(placement: .automatic) {
+            cancelButton()
+        }
+        #endif
     }
 }
 

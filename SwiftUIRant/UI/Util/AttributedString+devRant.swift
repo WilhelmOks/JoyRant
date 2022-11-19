cac377ac07c2d6c491706f19f7082b5ae1829712
Wilhelm Oks <>
2022-11-19 12:42:17 +0100

    added LinkInRantType

--- SwiftUIRant/UI/Util/AttributedString+devRant.swift
+++ SwiftUIRant/UI/Util/AttributedString+devRant.swift
@@ -8,24 +8,30 @@
 import Foundation
 import SwiftRant
 
+//TODO: implement in SwiftRant
+enum LinkInRantType: String {
+    case url = "url"
+    case mention = "mention"
+}
+
 extension AttributedString {
     static func from(postedContent: String, links: [Rant.Link]?) -> AttributedString {
         var result = AttributedString(stringLiteral: postedContent)
         
-        //TODO: handle devrant links
-        
         links?.forEach { link in
             if let range = result.range(of: link.title) {
-                //TODO: make an enum for link.type
-                if link.type == "url" {
+                switch LinkInRantType.init(rawValue: link.type) {
+                case .url:
                     result[range].foregroundColor = .primaryForeground
                     result[range].underlineStyle = .single
                     result[range].link = url(link: link.url)
-                } else if link.type == "mention" {
+                case .mention:
                     result[range].foregroundColor = .primary
                     result[range].font = .baseSize(16).bold()
                     result[range].swiftUI.font = .baseSize(16).bold()
                     result[range].link = URL(string: link.url)
+                case nil:
+                    break
                 }
             }
         }
@@ -34,13 +40,14 @@ extension AttributedString {
     }
     
     private static func url(link: String) -> URL? {
-        if link.hasPrefix("https://devrant.com/rants/") {
-            // a rant link looks like this:
+        let rantPrefix = "https://devrant.com/rants/"
+        if link.hasPrefix(rantPrefix) {
+            // A rant link looks like this:
             // https://devrant.com/rants/<rant-id>/first-few-letters-of-rant-content
-            // convert it into this:
+            // Convert it into this:
             // joyrant://rant/<rant-id>
             // so that it can be opened in this app.
-            let withCustomScheme = link.replacing("https://devrant.com/rants/", with: "joyrant://rant/")
+            let withCustomScheme = link.replacing(rantPrefix, with: "joyrant://rant/")
             let components = withCustomScheme.split(separator: "/", omittingEmptySubsequences: false)
             let joinedUntilRantId = components.prefix(upTo: 4).joined(separator: "/")
             return URL(string: joinedUntilRantId)

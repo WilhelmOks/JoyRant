625d558831a58de2f9081eafebb1dc58f32b1ee4
Wilhelm Oks <oksw@Mac.fritz.box>
2026-01-09 15:45:44 +0100

    no longer ignoring cert errors

--- SwiftUIRant/Networking.swift
+++ SwiftUIRant/Networking.swift
@@ -13,7 +13,7 @@ import SpamDetector
 struct Networking {
     static let shared = Self()
     
-    private let devRant = DevRantRequest(requestLogger: RequestLogger(), ignoreCertificateErrors: true)
+    private let devRant = DevRantRequest(requestLogger: RequestLogger(), ignoreCertificateErrors: false)
     
     private init() {}
     
